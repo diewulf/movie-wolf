1164d2f70c36ee3d5ee155f3435bfe19a7d055b6
diewulf <zero_variant@hotmail.com>
2025-05-27 20:04:35 -0400

    uploading movies

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
 import './../globals.css';
 import Header from '@/components/layout/header';
-import SearchInput from '@/components/search';
+import SearchInput, { Movie } from '@/components/search';
 import { MOVIES } from './movie-list';
 
 export default function RootLayout({
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <>
       <Header>
-        <SearchInput movies={MOVIES} />
+        <SearchInput movies={MOVIES as Movie[]} />
       </Header>
       <div className="flex-1 container mx-auto px-4 py-6">{children}</div>
     </>
